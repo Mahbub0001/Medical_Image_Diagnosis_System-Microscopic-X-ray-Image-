c63cdd30c2fec3806d620bb76f6482072fe18d36
Mahbub0001 <nibirbhuiyan18@gmail.com>
2026-06-02 15:52:28 +0600

    ui2(blood smear name)

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -16,7 +16,7 @@ const links = [
   },
   { 
     to: "/upload", 
-    label: "Upload Image",
+    label: "Blood Smear",
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
         <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
